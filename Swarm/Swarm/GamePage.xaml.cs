852702ba0bb616a316b4576f8c85ab43e59c0f6a
Astrohelo <axelvgames@gmail.com>
2023-11-19 23:27:10 +0100

    Most basic hexagon drawing that are clickable

--- Swarm/Swarm/GamePage.xaml.cs
+++ Swarm/Swarm/GamePage.xaml.cs
@@ -1,7 +1,15 @@
-namespace Swarm;
 
-public partial class GamePage : ContentPage {
-    public GamePage() {
-        InitializeComponent();
+using Microsoft.Maui.Controls.Shapes;
+
+namespace Swarm {
+    public partial class GamePage : ContentPage {
+        public GamePage() {
+            InitializeComponent();
+        }
+        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e) {
+            var polygon = (Polygon)sender;
+            polygon.Fill = new SolidColorBrush(Colors.Red);
+        }
     }
+
 }
