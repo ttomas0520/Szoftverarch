70a4c8dc4ddd76aec04a5ef7ef759be7e3950130
Astrohelo <axelvgames@gmail.com>
2023-11-24 14:53:57 +0100

    Added to logic classes

--- SwarmWPF/SwarmWPF/Logic/Hex.cs
+++ SwarmWPF/SwarmWPF/Logic/Hex.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SwarmWPF.Logic {
-    class Hex {
+    public class Hex {
         public int Row { get; set; }
         public int Column { get; set; }
         public bool HasAnt { get; set; }
+        public bool IsReserved { get; set; }
+        public List<Hex> Neighbours { get; set; }
+        public int Priority { get; set; }
 
         public Hex() { }
+
+        public void ReserveNext() {
+            if (!HasAnt) return;
+        }
     }
 }
