1d6cdb26ad9e3a36c128af608798745900d4ba8a
Astrohelo <axelvgames@gmail.com>
2023-11-24 23:13:07 +0100

    Add hexes their neighbours

--- SwarmWPF/SwarmWPF/Logic/Board.cs
+++ SwarmWPF/SwarmWPF/Logic/Board.cs
@@ -35,8 +35,39 @@ namespace SwarmWPF.Logic {
                 }
                 hexList.Add(rowList);
             }
-
+            ConnectHexNeighbors();
             return hexList;
+        }
+
+        private void ConnectHexNeighbors() {
+            for (int i = 0; i < Row; i++) {
+                for (int j = 0; j < Column; j++) {
+                    Hex hex = HexList[i][j];
+                    hex.Neighbours = GetHexNeighbors(i, j);
+                }
+            }
+        }
+
+        private List<Hex> GetHexNeighbors(int row, int column) {
+            List<Hex> neighbors = new List<Hex>();
+
+            // Define the six possible neighbors' coordinates
+            int[,] neighborCoords = {
+            { -1, 0 }, { -1, 1 }, { 0, 1 },
+            { 1, 0 }, { 1, -1 }, { 0, -1 }
+        };
+
+            for (int k = 0; k < 6; k++) {
+                int neighborRow = row + neighborCoords[k, 0];
+                int neighborColumn = column + neighborCoords[k, 1];
+
+                // Check if the neighbor coordinates are within the board bounds
+                if (neighborRow >= 0 && neighborRow < Row && neighborColumn >= 0 && neighborColumn < Column) {
+                    neighbors.Add(HexList[neighborRow][neighborColumn]);
+                }
+            }
+
+            return neighbors;
         }
     }
 }
