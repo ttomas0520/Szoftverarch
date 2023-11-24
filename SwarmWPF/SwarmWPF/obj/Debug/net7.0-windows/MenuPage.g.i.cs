d845c23a28a952d3107bd8fe11edb2c973fa83e5
ttomas0520 <79412823+ttomas0520@users.noreply.github.com>
2023-11-24 16:43:38 +0100

    3 control buttons

--- SwarmWPF/SwarmWPF/obj/Debug/net7.0-windows/MenuPage.g.i.cs
+++ SwarmWPF/SwarmWPF/obj/Debug/net7.0-windows/MenuPage.g.i.cs
@@ -63,13 +63,13 @@ namespace SwarmWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SwarmWPF;component/menupage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SwarmWPF;V1.0.0.0;component/menupage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MenuPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -79,7 +79,7 @@ namespace SwarmWPF {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
